189a0f3249b40de7c6e1c2b38f95b9296399e09e
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-20 17:37:41 +0000

    new engineer button added and working

--- src/main/java/controllers/EngineersController.java
+++ src/main/java/controllers/EngineersController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import db.DBHelper;
+import models.Department;
 import models.Engineer;
 import models.Manager;
 import spark.ModelAndView;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class EngineersController {
 
@@ -25,6 +27,26 @@ public class EngineersController {
             model.put("template", "templates/engineers/index.vtl");
             model.put("engineers", engineers);
             return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/engineers/new", (req, res)-> {
+            Map<String, Object> model = new HashMap<>();
+            List<Department> departments = DBHelper.getAll(Department.class);
+            model.put("template", "templates/engineers/create.vtl");
+            model.put("departments", departments);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/engineers", (req, res)-> {
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            Department department = DBHelper.find(departmentId, Department.class);
+            String firstName = req.queryParams("firstName");
+            String lastName = req.queryParams("lastName");
+            int salary = Integer.parseInt(req.queryParams("salary"));
+            Engineer engineer = new Engineer(firstName, lastName, salary, department);
+            DBHelper.save(engineer);
+            res.redirect("/engineers");
+            return null;
         }, new VelocityTemplateEngine());
 
     }
